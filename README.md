85acd59988dc3161ee022cba275f4763ce9de128
Victor Luis <112185132+victorluisbr1@users.noreply.github.com>
2024-04-22 11:10:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-relogio
+ Este projeto consiste em um relógio simples implementado em JavaScript puro. Ele exibe a hora atual e permite que você inicie, pause e zere o tempo.
---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+let segundos = 0;
+let timer;
+
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        });
+    };
+    
+    function iniciaRelogio () {
+        timer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000)
+    }
+    
+    document.addEventListener('click', function(e) {
+        const el = e.target;
+    
+        if(el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+    
+        if(el.classList.contains('pausar')) {
+            relogio.classList.add('pausado');
+            clearInterval(timer);
+        }
+    
+        if(el.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0; 
+        }
+    });
+}
+    relogio();
+    
+    // iniciar.addEventListener('click', function (event){
+    //     relogio.classList.remove('pausado');
+    //     clearInterval(timer);
+    //     iniciaRelogio();
+    // });
+    
+    // pausar.addEventListener('click', function (event) {
+    //     relogio.classList.add('pausado');
+    //     clearInterval(timer);
+    // });
+    
+    // zerar.addEventListener('click', function (event) {
+    //     clearInterval(timer);
+    //     relogio.innerHTML = '00:00:00';
+    //     segundos = 0;
+    // })
+
